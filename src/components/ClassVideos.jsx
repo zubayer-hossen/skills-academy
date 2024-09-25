29668d77bb9690157fc60b8296a41bfbf752d6d6
ZUBAYER HOSSEN <125750411+zubayer-hossen@users.noreply.github.com>
2024-09-25 09:51:15 -0700

    Update ClassVideos.jsx
    Commit9

--- src/components/ClassVideos.jsx
+++ src/components/ClassVideos.jsx
@@ -12,7 +12,32 @@ const videos = [
     description: "This is the second class video.",
     url: "https://drive.google.com/file/d/1zmI_hn0RbHalh1hgCxgYyvDC6NrKzUZx/preview",
   },
-
+  {
+    title: "Class Video 3",
+    description: "This is the Third class video.",
+    url: "https://drive.google.com/file/d/1zmI_hn0RbHalh1hgCxgYyvDC6NrKzUZx/preview",
+  },
+{
+    title: "Class Video 6",
+    description: "This is the 6th class video.",
+    url: "https://drive.google.com/file/d/14syfkAUcC--4C05gCv3BVOI6J_naDc_k/preview",
+  },
+  
+  {
+    title: "Class Video 7",
+    description: "This is the 7th class video.",
+    url: "https://drive.google.com/file/d/1YJ8EAteHc5A2C50S6KAo4aCHUz0YLkPp/preview",
+  },
+  {
+    title: "Class Video 8",
+    description: "This is the 8th class video.",
+    url: "https://drive.google.com/file/d/1_ZJdjFGOYsnYva3AJhmHvKyIaYgNdmkc/preview",
+  },
+  {
+    title: "Class Video 9",
+    description: "This is the 9th class video.",
+    url: "https://drive.google.com/file/d/1gl4FToDdtpziFcPK1AHh3HLg1lKSzOBx/preview",
+  },
   // Add more videos here
 ];
 
